dffd33af91336589e9f23de0e1798c45f6dc85b4
Dageus <jomouzio@gmail.com>
2026-04-13 23:12:12 +0100

    feat: semi-functional config, not happy with waybar yet

--- modules/flake-module.nix
+++ modules/flake-module.nix
@@ -4,37 +4,36 @@ let
     List the children of `dir`.
 
     # Inputs
-
-    `dir`
-
-    : Directory to read
+    `dir`: Directory to read
 
     # Type
-
     ```
     children :: path -> [path]
     ```
   */
   children =
     dir:
-    with builtins;
-    lib.pipe dir [
-      readDir
-      attrNames
+    let
+      contents = builtins.readDir dir;
+
+      # Helper to decide if a file/folder is actually a valid Nix module
+      isValidModule =
+        name: type:
+        (type == "regular" && lib.hasSuffix ".nix" name)
+        || (type == "directory" && builtins.pathExists (dir + "/${name}/default.nix"));
+    in
+    lib.pipe contents [
+      (lib.filterAttrs isValidModule)
+      builtins.attrNames
       (map (name: dir + "/${name}"))
     ];
 
   /**
     Wrap all files in `dir` as submodule imports.
-
     # Inputs
-
-    `dir`
-
-    : Directory to package as a module
+    `dir`: Directory to package as a module
 
     # Type
-
     ```
     wrap :: path -> AttrSet
     ```
