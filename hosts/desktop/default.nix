1fc83aabbe862ad535cda4ac91da176cabd13cc2
Dageus <jomouzio@gmail.com>
2026-08-01 22:04:57 +0100

    feat: desktop hardware config + basic test

---
+++ hosts/desktop/default.nix
@@ -0,0 +1,94 @@
+{
+  inputs,
+  profiles,
+  pkgs,
+  ...
+}:
+{
+  my.user.name = "jomouzio";
+  my.user.fullName = "Jomouzio";
+  my.system.hostName = "laptop";
+
+  my.secrets.sops = {
+    enable = true;
+  };
+
+  imports = with profiles; [
+    inputs.hardware.nixosModules.common-cpu-intel
+    inputs.hardware.nixosModules.common-pc-laptop-ssd
+    "${inputs.hardware}/common/cpu/intel/kaby-lake"
+    ./hardware-configuration.nix
+    ./disks.nix
+    ./home.nix
+
+    # Profiles =================================================================
+
+    laptop.sleep
+
+    hardware.bluetooth
+    hardware.battery
+    hardware.zram
+
+    editors.nvim
+
+    system.boot.systemd
+
+    system.nix-config
+    system.nix-ld
+    system.nix-helper
+    system.audio
+    system.locales
+    system.swappiness
+
+    services.ssh
+
+    graphical.niri
+    graphical.xdg
+    graphical.stylix
+    graphical.file-explorer
+    graphical.noctalia
+    graphical.vicinae
+    graphical.kitty
+    graphical.firefox
+    graphical.spotify
+    # graphical.syncthing
+    graphical.discord
+    graphical.mpv
+
+    greeter.sddm
+
+    shell.direnv
+    shell.fastfetch
+    shell.git
+    shell.nix-index
+    shell.tmux
+    shell.utils
+    shell.yazi
+    shell.zoxide
+    shell.zsh
+
+    virtualization.docker
+
+    networking.tailscale
+    networking.trust-lan
+    networking.networkmanager
+  ];
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.iosevka
+  ];
+
+  hm.home.packages = with pkgs; [
+    arandr
+    ani-cli
+    stremio-linux-shell
+  ];
+
+  boot.initrd.availableKernelModules = [
+    "usb_storage"
+    "sd_mod"
+  ];
+
+  # DO NOT CHANGE: https://wiki.nixos.org/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "23.11";
+}
