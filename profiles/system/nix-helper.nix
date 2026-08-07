fbd50e1d1ca85c2c631d3910c9d6612d13c7bd2e
Dageus <jomouzio@gmail.com>
2026-08-07 18:51:14 +0100

    chore: reduce lifetime of nix generations

--- profiles/system/nix-helper.nix
+++ profiles/system/nix-helper.nix
@@ -1,13 +1,13 @@
 { config, ... }:
 {
-  programs.nh = {
+  hm.programs.nh = {
     enable = true;
 
     # Automated garbage collection
     clean = {
       enable = true;
       dates = "weekly";
-      extraArgs = "--keep-since 10d --keep 10";
+      extraArgs = "--keep-since 5d --keep 5";
     };
 
     flake = "/home/${config.my.user.name}/nix-config";
