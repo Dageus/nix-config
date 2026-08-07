89319d8ae52ee78d6f7123d1f7f9c6be14ad5f4d
Dageus <jomouzio@gmail.com>
2026-08-07 18:39:03 +0100

    feat: mpv now uses GPU-based decoder

--- profiles/graphical/mpv.nix
+++ profiles/graphical/mpv.nix
@@ -1,7 +1,10 @@
-{ ... }:
+{ config, ... }:
 {
 
   hm.programs.mpv = {
     enable = true;
+    config = {
+      hwdec = if config.my.hardware.nvidia.enable then "nvdec" else "auto";
+    };
   };
 }
